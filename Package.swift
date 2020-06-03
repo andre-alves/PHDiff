1528137fad8bd2ecc6becd55b4210723e4bb212a
Max Chuquimia <Max.Chuquimia@foxsports.com.au>
2020-06-03 14:35:06 +1000

    Update to Swift 5.2

--- Package.swift
+++ Package.swift
@@ -1,3 +1,28 @@
+// swift-tools-version:5.2
+
 import PackageDescription
 
-let package = Package(name: "PHDiff", dependencies : [], exclude: ["Tests"])
+let package = Package(
+    name: "PHDiff",
+    products: [
+        .library(
+            name: "PHDiff",
+            targets: [
+                "PHDiff"
+            ]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "PHDiff",
+            path: "Sources"
+        ),
+        .testTarget(
+            name: "PHDiffTests",
+            dependencies: [
+                "PHDiff"
+            ],
+            path: "Tests"
+        ),
+    ]
+)
